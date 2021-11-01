d1be81226a2f14e9a328b2553c38274b92a987c2
faris <>
2021-11-01 23:05:38 +0400

    no message

---
+++ mvvm_sample/mvvm_sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm_sample
+//
+//  Created by Faris Muhammed on 01/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
